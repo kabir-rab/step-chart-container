11b380d4dc286a80b4986ea0bcb412d52b8991ba
kabir-rab <kabir-rab@users.noreply.github.com>
2020-06-04 17:32:25 +0100

    v1.1 changes
    added 3 new features.

--- step-chart-container.js
+++ step-chart-container.js
@@ -24,24 +24,60 @@ define( [
 			
 			controller: ['$scope', function ( $scope ) {
 				var app = qlik.currApp(this);									
-
+				var firstRender = true;
 				$scope.totalSteps = function(){var t; t=$scope.layout.listItems.length; return t};
 				$scope.extId = '#'+$scope.layout.qInfo.qId+'-chart';
 				$scope.avatarId = $scope.layout.qInfo.qId+'-avatar';
 				$scope.textId = $scope.layout.qInfo.qId+'-text';
 				$scope.liClass = $scope.layout.qInfo.qId+'-css';
-											
+				$scope.showExport = false;
+				//$scope.currentStep = 0;
+				
 				$scope.stepMasterItem = function(maserItemNo){
 					//qlik.theme.apply('card');																	
-					app.getObject( $scope.extId, $scope.layout.listItems[maserItemNo].props.masteritem );				
+					var chartExport = $scope.layout.listItems[maserItemNo].props.export;
+					app.getObject( $scope.extId, $scope.layout.listItems[maserItemNo].props.masteritem );
+									
 					$("[id^="+$scope.liClass+"]").removeClass("active");					
 					$('#'+$scope.liClass+'-'+maserItemNo).addClass("active");
 					
 					$("[id^="+$scope.avatarId+"]").addClass("qv-object-step-chart-container-not-select");
 					$('#'+$scope.avatarId+'-'+maserItemNo).removeClass("qv-object-step-chart-container-not-select");
 					$('#'+$scope.avatarId+'-'+maserItemNo).addClass("qv-object-step-chart-container-select");
+
+					$('#qv-object-step-chart-export-btn').attr('steps', 'stepMasterItemExport('+maserItemNo+')');					
+					
+					$scope.showExport = chartExport;
+					$scope.currentStep = maserItemNo;
+					$scope.lastStep = $scope.totalSteps() - (maserItemNo+1);
 				};
+
+				$scope.next = function(){
+					if($scope.currentStep < ($scope.totalSteps()-1)){
+						$scope.stepMasterItem($scope.currentStep + 1)
+					}
+				}
+
+				$scope.prev = function(){
+					if($scope.currentStep > 0){
+						$scope.stepMasterItem($scope.currentStep - 1)
+					}
+				}
+
+				$scope.stepMasterItemExport = function(maserItemNo){									
+					app.visualization.get($scope.layout.listItems[maserItemNo].props.masteritem).then(function(vis){						
+						vis.exportData({format:'OOXML', state: 'A'}).then(function (link) {							
+							window.open(link);
+						});
+					});
+				};				
 				
+				$scope.firstRenderCall = function(){
+					if(firstRender == true){						
+						$scope.stepMasterItem(0);
+					}
+				};		
+
 				$scope.headerMinHeight = function(){
 					var minHeight,
 						heightPercen,
